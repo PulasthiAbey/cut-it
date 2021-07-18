90775cd8bce1ec15c4519b4d769ff5fea2906910
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-19 00:12:43 +0530

    Changes added

--- cutit/src/components/home.js
+++ cutit/src/components/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import {
-  Header,
+  Image,
   Button,
   Input,
   Icon,
@@ -17,8 +17,8 @@ const home = ({ navigation }) => {
   // declaring the grid view of the segment
 
   return (
-    <ScrollView style={styles.container}>
-     
+    <ScrollView>
+     <Image source={require('../assets/images/barber.jpg')}/>
 
       <View style={styles.widthSet} />
 
@@ -38,104 +38,6 @@ const home = ({ navigation }) => {
   );
 };
 
-const styles = StyleSheet.create({
-  padR: {
-    padding: 10,
-  },
-
-  listTitle: {
-    color: "#1970E6",
-    fontSize: 20,
-    marginLeft: 50,
-  },
-
-  switch: {
-    marginRight: 50,
-  },
-
-  widthSet: {
-    padding: 10,
-  },
-
-  iconImage: {
-    width: 100,
-    height: 100,
-  },
-
-  itemContainer: {
-    alignItems: "center",
-    padding: 10,
-    margin: 10,
-  },
-
-  flexrow: {
-    flexDirection: "row",
-    padding: 10,
-    marginRight: 10,
-  },
-
-  buttonContainerStyle: {
-    borderRadius: 50,
-  },
-
-  buttonStyle: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-  },
-
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    padding: 10,
-  },
-
-  headerStyle: {
-    color: "#1970E6",
-  },
-
-  input: {
-    height: 40,
-    margin: 12,
-  },
-
-  widthSet: {
-    padding: 10,
-  },
-
-  textView: {
-    fontWeight: "bold",
-    color: "#1970E6",
-    textAlign: "center",
-    fontSize: 16,
-  },
-
-  inputBlock: {
-    width: "50%",
-  },
-
-  background: {
-    backgroundColor: "#FFF",
-    flex: 1,
-  },
-
-  headline_text: {
-    color: "#1970E6",
-    fontSize: 16,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-
-  stylesItemLabel: {
-    color: "#1970E6",
-  },
-
-  tileGrid: {
-    color: "#1970E6",
-  },
-});
+const styles = StyleSheet.create({});
 
 export default home;
