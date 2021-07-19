90c2a8af535fa4a57a7e9f45ebc3d05c8f15cb24
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-19 14:39:46 +0530

    Changes

--- cutit/src/components/register.js
+++ cutit/src/components/register.js
@@ -22,44 +22,14 @@ const register = ({ navigation }) => {
   return (
     <ScrollView style={styles.container}>
       <HeaderItem />
-     
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  image: {
-    flex: 1,
-    justifyContent: "center",
-    opacity: 1,
-  },
-
   container: {
     flex: 1,
   },
-
-  mainContent: {
-    alignItems: "center",
-    backgroundColor: "#000000c0",
-  },
-
-  textItem: {
-    color: "white",
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-
-  subTextItem: {
-    color: "white",
-    fontSize: 21,
-    lineHeight: 42,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-
-  
 });
 
 export default register;
