4772e0ca765fd3baebc7a10ed41a246976096c5e
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-19 11:58:36 +0530

    Changes

--- cutit/src/components/register.js
+++ cutit/src/components/register.js
@@ -6,16 +6,23 @@ import {
   ImageBackground,
   ScrollView,
   TouchableOpacity,
+  Dimensions,
 } from "react-native";
 import { Button } from "react-native-elements";
 
 import styleItem from "../styles/styles";
 import HeaderItem from "../utils/head";
 
+const { width } = Dimensions.get("screen");
+
+const ITEM_WIDTH = width * 0.9;
+const ITEM_HEIGHT = ITEM_WIDTH * 0.9;
+
 const register = ({ navigation }) => {
   return (
     <ScrollView style={styles.container}>
       <HeaderItem />
+     
     </ScrollView>
   );
 };
@@ -51,6 +58,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
+
+  
 });
 
 export default register;
