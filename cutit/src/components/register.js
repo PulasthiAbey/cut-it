2ecdcbae29982d48c56ed65fcfed6ac67d7535d9
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-22 19:40:48 +0530

    chnages

---
+++ cutit/src/components/register.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+  Animated,
+} from "react-native";
+import { Button } from "react-native-elements";
+import styleItem from "../styles/styles";
+
+const register = ({ navigation }) => {
+  return (
+      <View style={styleItem.container}>
+          
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default register;
